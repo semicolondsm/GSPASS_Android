1e11fa6bd1810f82acc73ba9c4d5272c24d9d7b6
minjun8946 <minjun8946@daum.net>
2021-05-31 08:58:43 +0900

    📑 :: gspass logo

--- app/src/main/java/com/example/gspass_android/ui/LoginActivity.kt
+++ app/src/main/java/com/example/gspass_android/ui/LoginActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gspass_android.R
 
-class Login : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
