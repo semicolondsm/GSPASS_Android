383565c683d373f020220873691180d237e49f24
minjun8946 <minjun8946@daum.net>
2021-06-06 15:00:48 +0900

    ⚡️ ::  오늘의 급식 날짜

--- app/src/main/java/com/example/gspass_android/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/gspass_android/ui/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.gspass_android.ui.activity
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
 import com.example.gspass_android.R
@@ -9,6 +11,9 @@ import com.example.gspass_android.adapter.MealAdapter
 import com.example.gspass_android.databinding.ActivityMainBinding
 import com.example.gspass_android.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.time.LocalDate
+import java.time.LocalDateTime
+import kotlin.time.days
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,34 +21,32 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModel()
 
-
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var viewPager2: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.vm = viewModel
         binding.lifecycleOwner = this
         setContentView(binding.root)
 
         val mealAdapter = MealAdapter(viewModel)
         mealAdapter.pos.value?.let { viewModel.meals(0) }
-        mealAdapter.pos.value?.let { viewModel.meals(-1) }
-        mealAdapter.pos.value?.let { viewModel.meals(1) }
 
-
-        mealAdapter.pos.observe(this,{
+        mealAdapter.pos.observe(this, {
             println("이게 문제가 맞나요???????")
             mealAdapter.pos.value?.let { viewModel.meals(it) }
+            println("$it 날짜 날짜")
         })
 
         viewPager2 = findViewById(R.id.meal_viewpager)
 
         viewPager2.adapter = mealAdapter
         viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-        viewPager2.setCurrentItem(currentPosition,true)
+        viewPager2.setCurrentItem(currentPosition, false)
 
-        viewModel.successEvent.observe(this,{
+        viewModel.successEvent.observe(this, {
             println("성공성공성공${viewModel.morningMenu.value}")
         })
     }
--- app/src/main/java/com/example/gspass_android/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/gspass_android/viewmodel/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.gspass_android.viewmodel
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import com.example.gspass_android.BaseApi
 import com.example.gspass_android.base.BaseViewModel
@@ -10,6 +12,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
 import retrofit2.HttpException
+import java.time.LocalDate
 
 class MainViewModel(
     private val sharedPreferences: LocalStorage,
@@ -38,42 +41,51 @@ class MainViewModel(
     val dinnerMenu4 = MutableLiveData<String>()
     val dinnerMenu5 = MutableLiveData<String>()
 
+    val today = MutableLiveData<String>()
 
     private val accessToken = sharedPreferences.getAccessToken()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun meals(day: Int) {
+        today.value = getDate(day)
         println("$accessToken asdfadfad")
         val apiResult = baseApi.meals(accessToken, "$day")
 
         val disposableSingleObserver = object : DisposableSingleObserver<MealsData>() {
+            @RequiresApi(Build.VERSION_CODES.O)
             override fun onSuccess(t: MealsData) {
+                getDate(0)
                 println("데이 ${day}")
-                when(day%5){
+                when(day % 5){
                     0->{
                         morningMenu.value = getMealsString(t.breakfast)
                         lunchMenu.value = getMealsString(t.lunch)
                         dinnerMenu.value = getMealsString(t.dinner)
+                        println("111111111111")
                     }
                     -1,1 ->{
                         morningMenu2.value = getMealsString(t.breakfast)
                         lunchMenu2.value = getMealsString(t.lunch)
                         dinnerMenu2.value = getMealsString(t.dinner)
-
+                        println("22222222222222")
                     }
                     2,-2->{
                         morningMenu3.value = getMealsString(t.breakfast)
                         lunchMenu3.value = getMealsString(t.lunch)
                         dinnerMenu3.value = getMealsString(t.dinner)
+                        println("33333333333333")
                     }
                     3,-3->{
                         morningMenu4.value = getMealsString(t.breakfast)
                         lunchMenu4.value = getMealsString(t.lunch)
                         dinnerMenu4.value = getMealsString(t.dinner)
+                        println("444444444444444")
                     }
                     else ->{
                         morningMenu5.value = getMealsString(t.breakfast)
                         lunchMenu5.value = getMealsString(t.lunch)
                         dinnerMenu5.value = getMealsString(t.dinner)
+                        println("5555555555555555")
                     }
                 }
                 successEvent.setValue(Unit)
@@ -96,6 +108,16 @@ class MainViewModel(
         addDisposable(observer)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getDate(day : Int): String {
+        val today : LocalDate = LocalDate.now()
+        println("${today.dayOfMonth} ${today.monthValue} 가나다라")
+        val date =  today.dayOfMonth
+        val month = today.monthValue
+        val reDay = "$month 월$date 일"
+        return reDay
+    }
+
     fun getMealsString(meals: ArrayList<String>): String {
         val size = meals.size
         var mealString = ""
@@ -108,7 +130,6 @@ class MainViewModel(
         else{
             mealString = "급식이 없습니다 ㅜ"
         }
-
         return mealString
     }
 }
